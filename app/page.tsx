625c1c13f5d0a557f31d668a168e2670931a7627
almoallemlamas <elmuallem994@gmail.com>
2024-08-30 00:15:08 +0300

    ddddde

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         </span>
       </h1>
       {/* توضيح الأزرار */}
-      <p className="mb-6 text-center text-sm font-extrabold md:text-base lg:text-lg text-red-500">
+      <p className="mb-6 text-center text-lg font-bold  text-red-500">
         اضغط على الزر لتحميل التطبيق
       </p>
       <div className="flex flex-row space-x-4">
@@ -36,7 +36,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <button className="flex flex-col items-center p-6 bg-green-500 text-white rounded-xl shadow-2xl hover:shadow-3xl hover:bg-green-600 transition duration-200 transform hover:scale-110 active:scale-95">
-            <FaAndroid className="w-16 h-16 lg:w-20 lg:h-20 mb-2 animate-bounce" />
+            <FaAndroid className="w-20 h-20  mb-2 animate-bounce" />
             <span className="text-base md:text-lg lg:text-xl">Android</span>
           </button>
         </Link>
@@ -48,7 +48,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <button className="flex flex-col items-center p-6 bg-blue-500 text-white rounded-xl shadow-2xl hover:shadow-3xl hover:bg-blue-600 transition duration-200 transform hover:scale-110 active:scale-95">
-            <FaApple className="w-16 h-16 lg:w-20 lg:h-20 mb-2 animate-bounce" />
+            <FaApple className="w-20 h-20  mb-2 animate-bounce" />
             <span className="text-base md:text-lg lg:text-xl">iPhone</span>
           </button>
         </Link>
